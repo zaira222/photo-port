f621686209d0d039d8e337177ed89dd127ce33ee
ZP <zairaperez01@gmail.com>
2022-04-09 21:06:28 -0500

    created the Nav component

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import logo from './logo.svg';
+import Nav from './components/Nav';
+import About from './components/About';
+
+function App() {
+
+  return (
+    
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
