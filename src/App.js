2d1bb45ff22aa5abf071c45be1e49746be3df985
ZP <zairaperez01@gmail.com>
2022-04-12 21:56:09 -0500

    added functionality to contact form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from "./components/Gallery";
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -26,10 +27,9 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
-        </div>
       </main>
     </div>
   );
